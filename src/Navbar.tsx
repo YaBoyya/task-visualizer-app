75d1f1d2673c4923c469fe9c99cd7834b561355d
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-07-21 22:46:27 +0200

    Add initial navbar and remove unnecessary code from App component and css file

---
+++ src/Navbar.tsx
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+function Navbar(){
+  return (
+    <ul id='navbar'>
+      <li id='navbar-brand'>
+        task-visualizer-app
+      </li>
+      {/* <li id='navbar-item'>
+        Home
+      </li> */}
+    </ul>
+  );
+}
+
+export default Navbar
