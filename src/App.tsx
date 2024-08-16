510f5ba596518a29dbb64bceb96beeda0529d5df
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-08-16 16:03:44 +0200

    Add WIP MC_DZZZ algorithm with default values that can be launched with a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
 import './App.css'
 import Matrix from './Matrix'
+import MC_DZZZ from './MC_DZZZ'
+
+
+const taskGraph = [
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12
+  [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], // 0
+  [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0], // 1
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], // 2
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], // 3
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], // 4
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], // 5
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 6
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 8
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 9
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 10
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 11
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 12
+]
+
+const taskSpecification = [
+// pi, ai, Di -> czas wykonania zadania, liczba żądanych procesorów, najpóźniejszy możliwy termin zakończenia zadania
+  [30, 3, 90], // 0
+  [10, 2, 20], // 1
+  [5, 1, 5],   // 2
+  [10, 3, 30], // 3
+  [15, 1, 15], // 4
+  [20, 1, 20], // 5
+  [25, 3, 75], // 6
+  [25, 1, 25], // 7
+  [10, 1, 10], // 8
+  [20, 2, 40], // 9
+  [20, 3, 40], // 10
+  [10, 2, 20], // 11
+  [20, 3, 60]  // 12
+];
+const n = taskGraph.length; // task count
+const m = 3; // processor count
 
 function App() {
   return (
     <>
       <div className='container'>
         Hi hello
+        <button onClick={() => MC_DZZZ(taskGraph, taskSpecification, n, m)}>
+          Click me!
+        </button>
         <Matrix />
       </div>
     </>
---
+++ src/MC_DZZZ.tsx
@@ -0,0 +1,94 @@
+export interface EdgeParams {
+  start: number;
+  end: number;
+}
+
+function solveEdges(taskGraph: number[][]) {
+  let edges: EdgeParams[] = []; 
+
+  taskGraph.forEach((row, start) => {
+      row.forEach((el, end) => {
+      if (el == 1) {
+        let edge: EdgeParams = {
+          'start': start,
+          'end': end
+        } 
+        edges = [...edges, edge];
+      }
+    })
+  })
+  return edges
+}
+
+function solveTaskPriority(pi: number, ai: number) {
+  return pi*ai;
+}
+
+// TODO FIX all values are the same 
+function solveCriticalTaskTimes(startTask: number, taskGraph: number[][], taskSpecification: number[][]) {
+  let criticalTaskTime = taskSpecification[startTask][0];
+  let vals: number[] = [];
+  taskGraph[startTask].forEach((el, next) => {
+    if (el == 1) {
+      vals = [...vals, criticalTaskTime + solveCriticalTaskTimes(next, taskGraph, taskSpecification)];
+    }
+  }) 
+  console.log(`Task num: ${startTask}`)
+  console.log(Math.max(...vals, criticalTaskTime));
+  return Math.max(...vals, criticalTaskTime);
+}
+
+function solveHi(taskConnections: number[]) {
+  // TODO check if it's solved correctly -> edges to or from i 
+  // temporary solution, works as long as graph is just 0 and 1
+  return taskConnections.reduce((partialSum, a) => partialSum + a, 0);
+}
+
+function MC_DZZZ(tasksGraph: number[][], taskSpecification: number[][], n: number, m: number) {
+  // p: 0, a: 1, D: 2
+  const t = 0;
+
+  const edges = solveEdges(tasksGraph);       // E
+  const taskPriority = Array(n).fill(0);      // wi
+  const criticalTaskTimes= Array(n).fill(0);  // ti
+  const H = Array(n).fill(0);                 // hi
+
+  for (let i = 0; i < n; i++) {
+    taskPriority[i] = solveTaskPriority(taskSpecification[i][0], taskSpecification[i][1]);
+    criticalTaskTimes[i] = solveCriticalTaskTimes(i, tasksGraph, taskSpecification);
+    H[i] = solveHi(tasksGraph[i]);
+  }
+  
+  if (edges !== null) {
+    for (let i = 0; i < n; i++) {
+      taskPriority[i] = taskPriority[i] + criticalTaskTimes[i];
+    }
+  }
+  for (let i = 0; i < n; i++){
+    // TODO find new example that passes
+    if (criticalTaskTimes[i] > taskSpecification[i][2]) {
+      // TODO customize the event
+      throw Error(`Uszeregowanie nie istnieje.${i}, ${criticalTaskTimes[i]}, ${taskSpecification[i][2]}`);
+    }
+  }
+  const P = structuredClone(taskSpecification);    // T copy
+  const q = structuredClone(taskSpecification[0]); // pi copy
+  console.log(`Ilość tasków(n): ${n}`)
+  console.log(`Ilość procesów(w): ${m}`)
+  console.log(`Macierz specyfikacji zadań((pi, ai, Di), Ti ∈ T): ${taskSpecification}`)
+  console.log(`Zbiór krawędzi grafu(E): ${JSON.stringify(edges)}`)
+  console.log(`Priorytet zadania(w): ${taskPriority}`)
+  console.log(`Czas zadanie krytycznego(ti): ${criticalTaskTimes}`)
+  console.log(`Maksymalna liczba krawędzi w G prowadząca od Ti(h): ${H}`)
+  // console.log(`Priorytet zadania(w): ${W}`)
+  // while(P !== null) {
+  //   const av = m;
+  //   // Q = {Ti ∈ P : (qj = 0, j ∈ Bi ) ∧ (qi > 0)}; /*zbiór zadań gotowych*/
+  //   if (Q === null) {
+  //     // TODO goto end-while, to break czy continue?
+  //     break;
+  //   }
+  // }
+}
+
+export default MC_DZZZ;
