edc26358cc4bb64cb2e307afaca0974e57ecf171
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-12-01 15:30:12 +0100

    Add tsx and separate MC_DZZZ from app for now

--- src/MC_DZZZ.tsx
+++ src/MC_DZZZ.tsx
@@ -1,7 +1,49 @@
-import { getRandomValues } from "crypto";
-import { EdgeProps } from "./props";
+interface EdgeProps {
+  start: number;
+  end: number;
+}
 
+// inputs: 
+const taskGraph = [
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12
+  [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], // 0
+  [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0], // 1
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], // 2
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], // 3
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], // 4
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], // 5
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 6
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 8
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 9
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 10
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 11
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 12
+]
+  
+const taskSpecification = [
+// czas wykonania zadania, liczba żądanych procesorów, najpóźniejszy możliwy termin zakończenia zadania 
+// pi, ai, Di
+  [30, 3, 195], // 0
+  [10, 2, 155], // 1
+  [5,  1, 30],   // 2
+  [10, 3, 80], // 3
+  [15, 1, 105], // 4
+  [20, 1, 110], // 5
+  [25, 3, 135], // 6
+  [25, 1, 25], // 7
+  [10, 1, 50], // 8
+  [20, 2, 90], // 9
+  [20, 3, 40], // 10
+  [10, 2, 20], // 11
+  [20, 3, 60]  // 12
+];
+const n = taskGraph.length; // task count
+const m = 3; // processor count
+
+// code:
 function solveEdges(taskGraph: number[][]) {
+  // returns list of edges based on task graph
   let edges: EdgeProps[] = []; 
 
   taskGraph.forEach((row, start) => {
@@ -27,6 +69,7 @@ function getSecuredRandomFloat() {
 }
 
 function randRange(min: number, max: number) {
+  // returns cryptographically random value in range
   return Math.floor(getSecuredRandomFloat()*(max-min) + min)
 }
 
@@ -35,6 +78,7 @@ function solveTaskPriority(taskIndex: number, maxVerticesToTask: number[], taskS
     // Independent task on top of the tree
     return taskSpecification[taskIndex][0] * taskSpecification[taskIndex][1];
   }
+  // filters edges that have end on taskIndex and retruns their start value giving parents array
   const parents = edges.filter(item => item.end === taskIndex).map(item => item.start)
 
   // solves for maxProcessorCount accross all parents
@@ -59,45 +103,53 @@ function solveTaskPriority(taskIndex: number, maxVerticesToTask: number[], taskS
 
   // finally takes parent with lowest index
   return taskPriority[Math.min(...allMaxVertParents)] + taskSpecification[taskIndex][0] * taskSpecification[taskIndex][1];
+  
+  // old
   // const parentMaxProcessorCount = parents.reduce((parentMax, x, i, arr) => taskSpecification[x][1] > taskSpecification[parentMax][1] ? taskSpecification[x][1] : taskSpecification[parentMax][1], 0)
   // x > arr[parentMax] ? arr[i] : parentMax
-    // console.log(maxParentProcessorCount)
 }
 
 function solveCriticalTaskTimes(startTask: number, taskGraph: number[][], taskSpecification: number[][]) {
+  // first task value
   let criticalTaskTime = taskSpecification[startTask][0];
   let vals: number[] = [];
+  // check all possible vertices for starting node  
   taskGraph[startTask].forEach((el, next) => {
-    if (el == 1) {
+    if (el === 1) {
+      // if child exists adds its sum of current node and the child criticalTaskTime to the list
       vals = [...vals, criticalTaskTime + solveCriticalTaskTimes(next, taskGraph, taskSpecification)];
     }
-  }) 
+  });
+  // returns max from all the values and initial criticalTaskTime
   return Math.max(...vals, criticalTaskTime);
 }
 
 function solveMaxVerticesToTask(taskIndex: number, edges: EdgeProps[], maxVerticesToTask: number[]) {
+  // searches edges for parents od startIndex
   const parents = edges.filter(item => item.end === taskIndex).map(item => item.start);
 
   if (parents.length === 0) {
+    // if no parents return 0 as it's h = 0
     return 0;
   }
-
+  // check all parents for highest h value 
   const maxVertParent = parents.reduce((parentMax, x) => maxVerticesToTask[x] > maxVerticesToTask[parentMax] ? x : parentMax, parents[0]);
+  // increments parents h for the current node
   return maxVerticesToTask[maxVertParent] + 1;
 
   // old and most likely wrong
   // return taskGraph.reduce((partialSum, row) => partialSum + row[taskIndex], 0);
 }
 
-function findTaskIndex(taskArr, task) { 
+function findTaskIndex(taskArr: number[][], task: number[]) { 
   // TODO needs testing, should work, but most likely won't
-  console.log(taskArr.forEach((item) => item.every((el, i) => el[i] === task[i]) ? item : 0));
+  console.log(taskArr.forEach((item) => item.every((el: number, i: number) => el === task[i]) ? item : 0));
   return 0;
 }
 
 function MC_DZZZ(
   tasksGraph: number[][],         // G = (T, E), E ⊂ T × T
-  taskSpecification: number[][],  // ((pi, ai, Di), Ti ∈ T)
+  taskSpecification: number[][],  // ((p_i, a_i, D_i), Ti ∈ T);
   taskCount: number,              // n
   processorCount: number          // m
 ) {
@@ -105,36 +157,40 @@ function MC_DZZZ(
   let t = 0;
 
   const edges = solveEdges(tasksGraph);               // E
-  const taskPriority = Array(taskCount).fill(0);      // wi
-  const criticalTaskTimes= Array(taskCount).fill(0);  // ti
-  const maxVerticesToTask = Array(taskCount).fill(0); // hi
+  const taskPriority = Array(taskCount).fill(0);      // w_i
+  const criticalTaskTimes = Array(taskCount).fill(0); // t_i
+  const maxVerticesToTask = Array(taskCount).fill(0); // h_i
 
   for (let i = 0; i < taskCount; i++) {
     criticalTaskTimes[i] = solveCriticalTaskTimes(i, tasksGraph, taskSpecification);
     maxVerticesToTask[i] = solveMaxVerticesToTask(i, edges, maxVerticesToTask);
     taskPriority[i] = solveTaskPriority(i, maxVerticesToTask, taskSpecification, edges, taskPriority);
-    console.log(i, taskPriority[i])
   }
-  
+
+  console.log(`Priorytet zadania przed korektą dla zadań zależnych(w): ${taskPriority}`)
+
   if (edges !== null) {
     for (let i = 0; i < taskCount; i++) {
       taskPriority[i] = taskPriority[i] + criticalTaskTimes[i];
     }
   }
+
   for (let i = 0; i < taskCount; i++){
     if (criticalTaskTimes[i] > taskSpecification[i][2]) {
       // TODO customize the event
       throw Error(`Uszeregowanie nie istnieje.${i}, ${criticalTaskTimes[i]}, ${taskSpecification[i][2]}`);
     }
   }
-  const availableTasks = maxVerticesToTask.map((el, index) => el === 0 ? index : -1).filter((el) => el !== -1);
+  const availableTasks = maxVerticesToTask.map((el, index) => el === 0 ? index : -1).filter((el) => el !== -1); // B
+
   const tasksCopy = structuredClone(taskSpecification);   // T copy (P)
   const taskTimeLength = taskSpecification.map(el => el[0]);   // pi copy (qi)
-  console.log(`Ilość tasków(n): ${taskCount}`)
+
+  console.log(`Ilość zadań(n): ${taskCount}`)
   console.log(`Ilość procesów(w): ${processorCount}`)
   console.log(`Macierz specyfikacji zadań((pi, ai, Di), Ti ∈ T): ${JSON.stringify(taskSpecification)}`)
   console.log(`Zbiór krawędzi grafu(E): ${JSON.stringify(edges)}`)
-  console.log(`Priorytet zadania(w): ${taskPriority}`)
+  console.log(`Priorytet zadania po korekcie dla zadań zależnych(w): ${taskPriority}`)
   console.log(`Czas zadanie krytycznego(ti): ${criticalTaskTimes}`)
   console.log(`Maksymalna liczba krawędzi w G prowadząca do Ti(h): ${maxVerticesToTask}`)
   console.log(`Dostępne zadania(B): ${availableTasks}`)
@@ -145,55 +201,68 @@ function MC_DZZZ(
     // })
   //   if (taskTimeLength[index] && )
 
+  console.log(tasksCopy, availableTasks, taskTimeLength)
   for (let i=0; i<tasksCopy.length; i++) {
-    if (taskTimeLength[availableTasks[i]] === 0 && taskTimeLength[i] > 0)
-      console.log(i) 
-    if (availableTasks.some((el, index) => taskTimeLength[el] === 0) && taskTimeLength[i] > 0)
-      console.log(`Hello ${i}`)
+    // (q_j = 0, j ∈ B_i )                                              ∧  (q_i > 0)
+    // console.log(`
+    //   id: ${i}\n
+    //   taskTimeLength[i]: ${taskTimeLength[i]}\n
+    //   availableTasks.every((el, index) => taskTimeLength[el] === 0: ${availableTasks.every((el, index) => taskTimeLength[el] === 0)}\n
+    //   taskTimeLength[i] > 0: ${taskTimeLength[i] > 0}
+    //   `)
+    if (availableTasks.some((el) => taskTimeLength[el] === 0) && taskTimeLength[i] > 0)
+      console.log(i);
   }
-  // while(P !== null) {
-    let av = processorCount;
-  //   // Q = {Ti ∈ P : (qj = 0, j ∈ Bi ) ∧ (qi > 0)}; /*zbiór zadań gotowych*/
-  //   if (Q === null) {
-  //     break;
-  //   }
-    // const M = Math.max(Q.map((taskIndex) => taskPriority[taskIndex])); // Max task priority in Q
-    // const W = Q.filter(taskIndex => taskPriority[taskIndex] === M);  // Tasks with M priority
-
-    // const A = Math.max(W.map((taskIndex) => taskSpecification[taskIndex][1])); // Max processors in W
-    // const F = W.filter(taskIndex => taskSpecification[taskIndex][1] === A);  // Tasks with A processors
-
-    // const C = Math.max(F.map((taskIndex) => maxVerticesToTask[taskIndex])); // Max vertices to task in F
-    // const J = F.filter(taskIndex => maxVerticesToTask[taskIndex] === C);  // Task with F vertices to them
-
-    // const chosenTask = J[randRange(0, J.length)];
-    // let x = 1;
-
-    // if (A < av) {
-    //   // usereguj Ti da jednej jednostki czasowej [t, t+1] ?
-    //   av -= chosenTask[1];
-    //   const chosenTaskIndex = findTaskIndex(tasksCopy, chosenTask);
-    //   taskTimeLength[chosenTaskIndex] -= x;
-    //   // TODO Q = Q − {Ti };
-    //   if (chosenTask[1] === 2) {
-    //     // TODO find(xj)
-    //     // TODO what is xj?
-    //   } 
-    //   if (chosenTask[1] === 2 /*&& xj === 1 */) {
-
-    //   }
-    // } else {
-    //   // TODO Q = Q − {Ti };
-    // }
-    // // TODO loop? if av > 0 go to step 14; /* gdy sa ̨ jeszcze wolne procesory w chwili t*/
-    // // {
-
-    // // } else {
-    // //   t += x;
-    // // }
-    // // TODO if qi = 0 then P = P − Ti ;
+  for (let i=0; i<tasksCopy.length; i++) {
+    if (availableTasks.every((el) => taskTimeLength[el] === 0) && taskTimeLength[i] > 0)
+      console.log(i);
+  }
+  while(tasksCopy !== null) {
+    let av: number = processorCount;
+    // Q = {Ti ∈ P : (qj = 0, j ∈ Bi ) ∧ (qi > 0)}; /*zbiór zadań gotowych*/
+    let Q = availableTasks;
+    let x: number = 1;
+    do{
+      if (Q === null) {
+        break;
+      }
+      const M = Math.max(...Q.map((taskIndex) => taskPriority[taskIndex])); // Max task priority in Q
+      const W = Q.filter(taskIndex => taskPriority[taskIndex] === M);  // Tasks with M priority
+  
+      const A = Math.max(...W.map((taskIndex) => taskSpecification[taskIndex][1])); // Max processors in W
+      const F = W.filter(taskIndex => taskSpecification[taskIndex][1] === A);  // Tasks with A processors
+  
+      const C = Math.max(...F.map((taskIndex) => maxVerticesToTask[taskIndex])); // Max vertices to task in F
+      const J = F.filter(taskIndex => maxVerticesToTask[taskIndex] === C);  // Task with F vertices to them
+  
+      const chosenTaskNumber = J[randRange(0, J.length)]
+      const chosenTask = taskSpecification[chosenTaskNumber];
+      x = 1;
     
-  // }
+      if (A < av) {
+        // usereguj Ti da jednej jednostki czasowej [t, t+1] ?
+        av -= chosenTask[1];
+        taskTimeLength[chosenTaskNumber] -= x;
+
+        console.log(Q, chosenTaskNumber)
+        Q = Q.filter(el => el !== chosenTaskNumber)
+        console.log(Q, chosenTaskNumber)
+        
+        // if (chosenTask[1] === 2 && getSecuredRandomFloat() > 0.9) {
+        //   // dodaj do zbioru T dodatkowe zadanie (T ′j gdzie aj = 3)
+        // }
+      } else {
+        console.log(Q, chosenTaskNumber)
+        Q = Q.filter(el => el !== chosenTaskNumber)
+        console.log(Q, chosenTaskNumber)
+      }
+    } while(av > 0)
+
+    t += x;
+    // // TODO if qi = 0 then P = P − Ti ;
+  }
 }
+
+MC_DZZZ(taskGraph, taskSpecification, n, m);
 
 export default MC_DZZZ;
